fa5f45bdf157a76f8e80e235301b9e16e06e9381
Anna <cainowa@gmail.com>
2024-03-26 00:15:33 +0100

    feat: add collage

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,37 @@
 import './style/main.scss';
+
+class Collage {
+  allImg: NodeListOf<Element>;
+
+  constructor() {
+    this.allImg = document.querySelectorAll('.collage__img');
+  }
+
+  collageAnimation() {
+    this.allImg.forEach(img => {
+      img.addEventListener('mouseenter', () => {
+        this.greyScaleAll(img);
+      });
+      img.addEventListener('mouseleave', () => {
+        this.resetGreyScale();
+      });
+    });
+  }
+
+  greyScaleAll(currentItem: Element): void {
+    this.allImg.forEach(item => {
+      if (item !== currentItem) {
+        item.classList.add('greyscale');
+      }
+    });
+  }
+
+  resetGreyScale() {
+    this.allImg.forEach(item => {
+      item.classList.remove('greyscale');
+    });
+  }
+}
+
+const collage = new Collage();
+collage.collageAnimation();
