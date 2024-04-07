ce56b146c408118f71b533ba7b5cbf6d4eb831eb
Anna <cainowa@gmail.com>
2024-04-07 15:56:09 +0200

    feat: add biography page

--- src/app/pages/Biography/biography.ts
+++ src/app/pages/Biography/biography.ts
@@ -15,7 +15,7 @@ export default class Biography {
 
     const bioText = document.createElement('p');
     bioText.classList.add(`biography__${this.name}-text`);
-    bioText.innerHTML = this.text;
+    bioText.innerHTML = this.text.replace(/\n/g, '<br>');
     bioBlock.appendChild(bioText);
 
     const bioImageBox = document.createElement('div');
