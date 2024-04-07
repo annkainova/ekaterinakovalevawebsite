11d5d862bf24a67c59097a36d3086a2e5cb046ba
annkainova <cainowa@gmail.com>
2024-04-07 17:30:24 +0200

    Feat/active page (#8)
    * feat: add active link
    
    * fix: fix links in header;

---
+++ src/app/pages/ActivePages/activePages.ts
@@ -0,0 +1,39 @@
+export default class ActiveLink {
+  currentPath: string;
+  linkIds: string[];
+
+  constructor() {
+    this.currentPath = window.location.pathname;
+    this.linkIds = [
+      'waiting-zone',
+      'maps',
+      'horizon-colonization',
+      'soup-of-the-day',
+      'early-works',
+      'antarctic-diary',
+      'memorial-objects',
+      'medical-history',
+      'notebooks',
+      'mosaics',
+      'artworks',
+    ];
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  setActiveLink() {
+    this.linkIds.forEach(id => {
+      const link = document.getElementById(id);
+      if (link) {
+        link.classList.remove('active');
+      }
+    });
+
+    const currentPage = this.linkIds.find(id => this.currentPath.endsWith(`${id}.html`));
+    if (currentPage) {
+      const link = document.getElementById(currentPage);
+      if (link) {
+        link.classList.add('active');
+      }
+    }
+  }
+}
--- src/app/pages/Main.ts
+++ src/app/pages/Main.ts
@@ -1,10 +1,12 @@
 import Collage from './Collage/Collage';
+import ActiveLink from './ActivePages/activePages';
 import Biography from './Biography/biography';
 import BiographyController from './Biography/BiographyController';
 import dataBiography from './Biography/dataBiography.json';
 
 export default class Main {
   collage: Collage;
+  activeLink: ActiveLink;
 
   bioView: Biography;
   personalView: Biography;
@@ -14,6 +16,9 @@ export default class Main {
   constructor() {
     this.collage = new Collage();
     this.collage.collageAnimation();
+
+    this.activeLink = new ActiveLink();
+    this.activeLink.setActiveLink();
 
     this.bioView = new Biography(
       dataBiography.bio.name,
