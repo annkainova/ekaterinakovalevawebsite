6cb640899f48863a925c639c4c4f8f4a2c08dec4
Anna <cainowa@gmail.com>
2024-04-08 20:07:43 +0200

    fix: fix mistake

--- src/index.ts
+++ src/index.ts
@@ -11,12 +11,14 @@ class AppInitializer {
 }
 
 AppInitializer.initialize();
+const container = document.querySelector('.your-gallery-container');
 
-Fancybox.bind('[data-fancybox="gallery"]', {
-  placeFocusBack: false,
+if (container instanceof HTMLElement) {
+  Fancybox.bind(container, '[data-fancybox="gallery"]', {
+    placeFocusBack: false,
 
-  tpl: {
-    main: `<div class="fancybox__container has-sidebar" role="dialog" aria-modal="true" aria-label="{{MODAL}}" tabindex="-1">
+    tpl: {
+      main: `<div class="fancybox__container has-sidebar" role="dialog" aria-modal="true" aria-label="{{MODAL}}" tabindex="-1">
     <div class="fancybox__backdrop"></div>
       <div class="fancybox__col">
         <div class="fancybox__carousel"></div>
@@ -24,72 +26,73 @@ Fancybox.bind('[data-fancybox="gallery"]', {
       
     </div>
   </div>`,
-  },
+    },
 
-  idle: false,
-  compact: false,
-  dragToClose: false,
+    idle: false,
+    compact: false,
+    dragToClose: false,
 
-  showClass: 'f-fadeIn',
-  hideClass: 'f-fadeOut',
+    showClass: 'f-fadeIn',
+    hideClass: 'f-fadeOut',
 
-  Images: {
-    zoom: false,
-  },
+    Images: {
+      zoom: false,
+    },
 
-  Thumbs: {
-    type: 'none',
-  },
-  // Toolbar: {
-  //   parentEl: toolbar => {
-  //     return toolbar.instance.container.querySelector('.fancybox__col');
-  //   },
-  //   items: {
-  //     sidebar: {
-  //       tpl: `<button class="f-button"><svg><circle cx="12" cy="12" r="10"/><path d="M12 16v-4M12 8h.01"/></svg></button>`,
-  //       click: toolbar => {
-  //         toolbar.instance.container.classList.toggle('has-sidebar');
-  //       },
-  //     },
-  //   },
-  //   display: {
-  //     left: [],
-  //     middle: [],
-  //     right: ['customButton'],
-  //   },
-  // },
+    Thumbs: {
+      type: 'none',
+    },
+    // Toolbar: {
+    //   parentEl: toolbar => {
+    //     return toolbar.instance.container.querySelector('.fancybox__col');
+    //   },
+    //   items: {
+    //     sidebar: {
+    //       tpl: `<button class="f-button"><svg><circle cx="12" cy="12" r="10"/><path d="M12 16v-4M12 8h.01"/></svg></button>`,
+    //       click: toolbar => {
+    //         toolbar.instance.container.classList.toggle('has-sidebar');
+    //       },
+    //     },
+    //   },
+    //   display: {
+    //     left: [],
+    //     middle: [],
+    //     right: ['customButton'],
+    //   },
+    // },
 
-  // on: {
-  //   'Carousel.ready Carousel.change': fancybox => {
-  //     const titleVal = fancybox.getSlide().triggerEl.dataset.title;
-  //     const titleEl = document.getElementById('fancybox_title');
+    // on: {
+    //   'Carousel.ready Carousel.change': fancybox => {
+    //     const titleVal = fancybox.getSlide().triggerEl.dataset.title;
+    //     const titleEl = document.getElementById('fancybox_title');
 
-  //     if (titleEl) {
-  //       titleEl.innerHTML = titleVal;
-  //     }
-  //   },
-  //   init: fancybox => {
-  //     fancybox.$container.querySelector('.is-close-btn').style.display = 'none';
-  //     const customBackButton = fancybox.$container.querySelector('.fancybox-button--customReturn');
-  //     if (customBackButton) {
-  //       customBackButton.addEventListener('click', () => {
-  //         fancybox.close();
-  //       });
-  //     }
-  //   },
-  // },
+    //     if (titleEl) {
+    //       titleEl.innerHTML = titleVal;
+    //     }
+    //   },
+    //   init: fancybox => {
+    //     fancybox.$container.querySelector('.is-close-btn').style.display = 'none';
+    //     const customBackButton = fancybox.$container.querySelector('.fancybox-button--customReturn');
+    //     if (customBackButton) {
+    //       customBackButton.addEventListener('click', () => {
+    //         fancybox.close();
+    //       });
+    //     }
+    //   },
+    // },
 
-  Toolbar: {
-    items: {
-      return: {
-        tpl: `<button class="f-button">вернуться</button>`,
-        click: () => {
-          Fancybox.close();
+    Toolbar: {
+      items: {
+        return: {
+          tpl: `<button class="f-button">вернуться</button>`,
+          click: () => {
+            Fancybox.close();
+          },
         },
       },
+      display: {
+        right: ['return'],
+      },
     },
-    display: {
-      right: ['return'],
-    },
-  },
-});
+  });
+}
