c0bc4e40fe589f5be65f70cd192149fab5d9ac08
annkainova <cainowa@gmail.com>
2024-04-09 16:34:31 +0200

    feat: add localization (#17)

---
+++ src/app/pages/Localization/Localization.ts
@@ -0,0 +1,22 @@
+export default class Localization {
+  language?: string;
+  buttonRu: HTMLElement | null;
+  buttonEn: HTMLElement | null;
+
+  constructor() {
+    this.buttonRu = document.querySelector('.language-switcher__option--ru');
+    this.buttonEn = document.querySelector('.language-switcher__option--en');
+
+    this.language = localStorage.getItem('language') || 'ru'; // Добавьте эту строку
+
+    this.buttonRu?.addEventListener('click', () => this.changeLanguage('ru'));
+    this.buttonEn?.addEventListener('click', () => this.changeLanguage('en'));
+  }
+
+  changeLanguage(lang: string) {
+    localStorage.setItem('language', lang);
+    this.language = localStorage.getItem('language');
+    // eslint-disable-next-line no-restricted-globals
+    location.reload();
+  }
+}
