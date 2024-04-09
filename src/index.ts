022f97c1182492035a06192024b1bbabeb89cc5f
annkainova <cainowa@gmail.com>
2024-04-09 10:12:23 +0200

    Feat/open photo (#13)
    * Feat/open photo (#10) (#11)
    
    * feat: add fancybox
    
    * Data.json filling (#9)
    
    * 09_antarctic-diary
    
    Added and translated part 09
    
    * 10_memorial-objects
    
    Added and translated
    
    * Update data.json
    
    Translation 3 block 05_maps 12_notebooks
    Checked 06 07 08 09 13 14
    
    * Add 10_right-to-rest
    
    added and translated
    
    * Quotation marks
    
    correction “”«» for ru and en
    
    ---------
    
    
    
    * maps path changed
    
    * mosaics path changed
    
    * horizon colonization path changed
    
    * artworks path changed
    
    * soup of the day path changed
    
    * early works path changed
    
    * antarctic diary path changed
    
    * notebooks path changed
    
    * memorial objects path changed
    
    * right to rest changed path
    
    * waiting zone path changed
    
    * feat: update setting and style for modal window
    
    * fix: fix data json file
    
    * fix: update folder name
    
    ---------
    
    Co-authored-by: Ilya Mirolubov <89379901+onesadkermit@users.noreply.github.com>
    Co-authored-by: Ilya Mirolubov <ilya_mirolubov@mail.ru>
    
    * fix: fix mistake
    
    * fix: turn off rules ts
    
    ---------
    
    Co-authored-by: Ilya Mirolubov <89379901+onesadkermit@users.noreply.github.com>
    Co-authored-by: Ilya Mirolubov <ilya_mirolubov@mail.ru>

--- src/index.ts
+++ src/index.ts
@@ -11,76 +11,33 @@ class AppInitializer {
 }
 
 AppInitializer.initialize();
-const container = document.querySelector('.your-gallery-container');
 
-if (container instanceof HTMLElement) {
-  Fancybox.bind(container, '[data-fancybox="gallery"]', {
+document.addEventListener('DOMContentLoaded', () => {
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-expect-error
+  Fancybox.bind('[data-fancybox="gallery"]', {
+    // Ваши настройки
     placeFocusBack: false,
-
     tpl: {
       main: `<div class="fancybox__container has-sidebar" role="dialog" aria-modal="true" aria-label="{{MODAL}}" tabindex="-1">
-    <div class="fancybox__backdrop"></div>
-      <div class="fancybox__col">
-        <div class="fancybox__carousel"></div>
-      </div>
-      
-    </div>
-  </div>`,
+              <div class="fancybox__backdrop"></div>
+              <div class="fancybox__col">
+                <div class="fancybox__carousel"></div>
+              </div>
+            </div>
+          </div>`,
     },
-
     idle: false,
     compact: false,
     dragToClose: false,
-
     showClass: 'f-fadeIn',
     hideClass: 'f-fadeOut',
-
     Images: {
       zoom: false,
     },
-
     Thumbs: {
       type: 'none',
     },
-    // Toolbar: {
-    //   parentEl: toolbar => {
-    //     return toolbar.instance.container.querySelector('.fancybox__col');
-    //   },
-    //   items: {
-    //     sidebar: {
-    //       tpl: `<button class="f-button"><svg><circle cx="12" cy="12" r="10"/><path d="M12 16v-4M12 8h.01"/></svg></button>`,
-    //       click: toolbar => {
-    //         toolbar.instance.container.classList.toggle('has-sidebar');
-    //       },
-    //     },
-    //   },
-    //   display: {
-    //     left: [],
-    //     middle: [],
-    //     right: ['customButton'],
-    //   },
-    // },
-
-    // on: {
-    //   'Carousel.ready Carousel.change': fancybox => {
-    //     const titleVal = fancybox.getSlide().triggerEl.dataset.title;
-    //     const titleEl = document.getElementById('fancybox_title');
-
-    //     if (titleEl) {
-    //       titleEl.innerHTML = titleVal;
-    //     }
-    //   },
-    //   init: fancybox => {
-    //     fancybox.$container.querySelector('.is-close-btn').style.display = 'none';
-    //     const customBackButton = fancybox.$container.querySelector('.fancybox-button--customReturn');
-    //     if (customBackButton) {
-    //       customBackButton.addEventListener('click', () => {
-    //         fancybox.close();
-    //       });
-    //     }
-    //   },
-    // },
-
     Toolbar: {
       items: {
         return: {
@@ -95,4 +52,4 @@ if (container instanceof HTMLElement) {
       },
     },
   });
-}
+});
