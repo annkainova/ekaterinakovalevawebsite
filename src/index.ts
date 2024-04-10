eebf3285fc0c2901f4619b5cbe7e1ec7dc9405ed
annkainova <cainowa@gmail.com>
2024-04-10 12:14:53 +0200

    Fix/bear animation (#22)
    * fix: work with bear animation
    
    * feat: walk left
    
    * fix: bear appear just one

--- src/index.ts
+++ src/index.ts
@@ -55,3 +55,25 @@ document.addEventListener('DOMContentLoaded', () => {
     },
   });
 });
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   const bear = document.querySelector('.splitter__element') as HTMLElement;
+//   const directions = ['left', 'right'];
+//   const delays = [0, 3000, 6000, 9000]; // in milliseconds
+
+//   // Randomize direction and delay
+//   const direction = directions[Math.floor(Math.random() * directions.length)];
+//   const delay = delays[Math.floor(Math.random() * delays.length)];
+
+//   // Set the bear's initial position and direction
+//   if (direction === 'left') {
+//     bear.style.animationName = 'bearWalkLeft';
+//     // bear.style.animationDuration = '25s';
+//   } else {
+//     bear.style.animationName = 'bearWalkRight';
+//   }
+//   // Show the bear after the randomized delay
+//   setTimeout(() => {
+//     bear.style.display = 'none';
+//   }, delay);
+// });
