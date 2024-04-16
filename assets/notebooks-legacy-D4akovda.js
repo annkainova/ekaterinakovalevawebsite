39d2d60c4f93e92d7b120ffb4f510ea06e0792a2
Anna <cainowa@gmail.com>
2024-04-16 23:09:41 +0200

    deploy

--- assets/notebooks-legacy-D4akovda.js
+++
@@ -1,2 +0,0 @@
-System.register(["./index-legacy-B06buF4G.js"],(function(e,t){"use strict";return{setters:[null],execute:function(){}}}));
-//# sourceMappingURL=notebooks-legacy-D4akovda.js.map
--- assets/soupOfTheDay-CqI69nLK.js
+++
@@ -1,2 +0,0 @@
-function t(){import.meta.url,import("_").catch((()=>1)),async function*(){}().next()}import"./index-DKzZg0uc.js";export{t as __vite_legacy_guard};
-//# sourceMappingURL=soupOfTheDay-CqI69nLK.js.map
